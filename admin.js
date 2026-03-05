6d256b9b26615cd056e62c0e6919438a3f502bb8
Johnni AI <johnniesue@a-1affordableplumbingservices.com>
2026-03-04 23:07:27 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,8 +2,8 @@ const SUPABASE_URL = "https://gwoirenrtxneamlzlgrf.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3b2lyZW5ydHhuZWFtbHpsZ3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Nzk4OTYsImV4cCI6MjA4ODI1NTg5Nn0.uEnMgMJvlsGW-xyaGBtZ0VWFLi-VKu27P8jI9UN7tUU"
 
 const db = supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_KEY
 )
 
 async function buildRoute(){
@@ -11,6 +11,11 @@ async function buildRoute(){
 const { data } = await db
 .from("pickup_addresses")
 .select("address")
+
+if(!data || data.length === 0){
+alert("No pickup addresses yet")
+return
+}
 
 let stops = data.map(x =>
 encodeURIComponent(x.address)
@@ -27,7 +32,7 @@ let url =
 `https://www.google.com/maps/dir/?api=1
 &origin=${church}
 &destination=${church}
-&waypoints=${waypoints}
+&waypoints=optimize:true|${waypoints}
 &travelmode=driving`
 
 window.open(url)
