375b3ba21a44a816307805b4efa4623086640a94
Johnni AI <johnniesue@a-1affordableplumbingservices.com>
2026-03-05 01:24:01 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,11 @@
 const SUPABASE_URL = "https://gwoirenrtxneamlzlgrf.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3b2lyZW5ydHhuZWFtbHpsZ3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Nzk4OTYsImV4cCI6MjA4ODI1NTg5Nn0.uEnMgMJvlsGW-xyaGBtZ0VWFLi-VKu27P8jI9UN7tUU"
 
+const db = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
+
 const churchLat = 32.9027
 const churchLng = -96.5639
 
