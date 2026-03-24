d8c54a7efdc15ff3b4a3d0b3f50752ce10562c3f
Johnni AI <johnniesue@a-1affordableplumbingservices.com>
2026-03-24 13:27:52 -0500

    Update admin.js : Map: remove church pin from preview map to avoid location mismatch confusion

--- admin.js
+++ admin.js
@@ -50,11 +50,6 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19
 }).addTo(map)
 
-L.marker([churchLat, churchLng])
-  .addTo(map)
-  .bindPopup("Church")
-  .openPopup()
-
 // ==============================
 // 📦 STATE
 // ==============================
