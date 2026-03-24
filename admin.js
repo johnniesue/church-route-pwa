f2ef1e602da4c456bf486cd2a3b23b6177197372
Johnni AI <johnniesue@a-1affordableplumbingservices.com>
2026-03-24 14:59:37 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -168,7 +168,7 @@ async function loadPendingPickupCount() {
     }
   }
 }
-}=========================
+//=========================
 // 🚚 DROP OFF
 // ==============================
 async function dropOff(id) {
